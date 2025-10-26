c7d8602e49bc1fd72a0b3f44c3d008a18e52cafb
hlq <justinjoslin@proton.me>
2025-10-26 00:01:09 -0500

    added core functionality

--- internal/teamdb/teamdb.go
+++ internal/teamdb/teamdb.go
@@ -3,6 +3,8 @@ package teamdb
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const DIRECTORY = "../../data/team.db"
@@ -15,20 +17,100 @@ func InitDatabase() {
 	defer db.Close()
 
 	create_user_db := `
-	CREATE TABLE IF NOT EXISTS users (
-		userid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL,
-		type INTEGER NOT NULL,
-		points INTEGER DEFAULT 0,
-		rank INTEGER DEFAULT -1,
-		token TEXT NOT NULL,
-		teamid INTEGER DEFAULT 0
+	CREATE TABLE IF NOT EXISTS teams (
+		teamid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name STRING NOT NULL
 	);
 	`
 
 	_, err = db.Exec(create_user_db)
 	if err != nil {
 		log.Printf("%q: %s\n", err, create_user_db)
+		return
+	}
+}
+
+func CreateTeam(name string) {
+	db, err := sql.Open("sqlite3", DIRECTORY)
+	if err != nil {
+		log.Println(err)
+		db.Close()
+		return
+	}
+	defer db.Close()
+
+	insert_team_sql := `
+	INSERT INTO teams (name)
+	VALUES (?)
+	`
+
+	_, err = db.Exec(insert_team_sql, name)
+	if err != nil {
+		log.Printf("%q: %s\n", err, insert_team_sql)
+		return
+	}
+}
+
+func DeleteTeam(teamid int) {
+	db, err := sql.Open("sqlite3", DIRECTORY)
+	if err != nil {
+		log.Println(err)
+		db.Close()
+		return
+	}
+	defer db.Close()
+
+	delete_team_sql := `
+	DELETE FROM teams WHERE teamid = ?
+	`
+
+	_, err = db.Exec(delete_team_sql, teamid)
+	if err != nil {
+		log.Printf("%q: %s\n", err, delete_team_sql)
+		return
+	}
+}
+
+// Retrieves team name from specified teamid
+func GetTeamName(teamid int) (string, error) {
+	db, err := sql.Open("sqlite3", DIRECTORY)
+	if err != nil {
+		log.Println(err)
+		db.Close()
+		return "", err
+	}
+	defer db.Close()
+
+	retrieve_teamname_sql := `
+	SELECT name FROM teams WHERE teamid = ?
+	`
+	var name string
+	err = db.QueryRow(retrieve_teamname_sql, teamid).Scan(&name)
+	if err != nil {
+		log.Printf("%q: %s\n", err, retrieve_teamname_sql)
+		return "", err
+	}
+	return name, nil
+}
+
+// Assign a new team name to a specified teamid.
+func AssignUserName(teamid int, name string) {
+	db, err := sql.Open("sqlite3", DIRECTORY)
+	if err != nil {
+		log.Println(err)
+		db.Close()
+		return
+	}
+	defer db.Close()
+
+	update_teamname_sql := `
+	UPDATE teams SET name = ?
+	WHERE teamid = ?
+	`
+
+	_, err = db.Exec(update_teamname_sql, name, teamid)
+	if err != nil {
+		log.Printf("%q: %s\n", err, update_teamname_sql)
 		return
 	}
 }
--- internal/teamdb/teamdb_test.go
+++ internal/teamdb/teamdb_test.go
@@ -1,1 +1,50 @@
 package teamdb
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
+
+func TestTeamDatabase(t *testing.T) {
+	const dbFile = "../../data/team.db"
+	os.Remove(dbFile)
+
+	// t.Cleanup(func() {
+	// 	err := os.Remove(dbFile)
+	// 	if err != nil {
+	// 		t.Logf("Warning: failed to remove previous database: %v", err)
+	// 	}
+	// })
+
+	// ------- Testing InitDatabase Function -------
+
+	InitDatabase()
+
+	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+		t.Errorf("Database file was not created")
+	}
+
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		t.Fatalf("Failed to open database file: %v", err)
+	}
+	defer db.Close()
+
+	var table_name string
+	query := "SELECT name FROM sqlite_master WHERE type='table' AND name='teams';"
+	err = db.QueryRow(query).Scan(&table_name)
+	if err != nil {
+		t.Fatalf("Failed to query database schema: %v", err)
+	}
+
+	if err == sql.ErrNoRows {
+		t.Fatal("Database file was created, but 'teams' table was not found.")
+	}
+	if err != nil {
+		t.Fatalf("Error while querying for 'teams' table: %v", err)
+	}
+	if table_name != "teams" {
+		t.Errorf("Found table, but name was incorrect. Got '%s', expected 'teams'", table_name)
+	}
+}
